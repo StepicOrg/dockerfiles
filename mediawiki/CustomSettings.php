3d0c416a43222e5efc317e47c1ff601d903e97fa
Kostya Charkin <93kostya@gmail.com>
2017-07-26 18:23:38 +0300

    add dockerfile for mediawiki

---
+++ mediawiki/CustomSettings.php
@@ -0,0 +1,57 @@
+<?php
+
+$wgGroupPermissions['*']['edit'] = false;
+$wgGroupPermissions['*']['createpage'] = false;
+$wgGroupPermissions['*']['createtalk'] = false;
+$wgGroupPermissions['*']['move'] = false;
+
+$wgGroupPermissions['user']['edit'] = true;
+$wgGroupPermissions['user']['createpage'] = true;
+$wgGroupPermissions['user']['createtalk'] = true;
+$wgGroupPermissions['user']['move'] = false;
+
+$wgGroupPermissions['sysop']['edit'] = true;
+$wgGroupPermissions['sysop']['createpage'] = true;
+$wgGroupPermissions['sysop']['createtalk'] = true;
+$wgGroupPermissions['sysop']['move'] = true;
+
+$wgAllowImageTag = true;
+
+$wgJobRunRate = 1000;
+$wgRunJobsAsync = false;
+
+## Extensions
+wfLoadExtension( 'WikiEditor' );
+
+$wgDefaultUserOptions['usebetatoolbar'] = 1;
+$wgDefaultUserOptions['usebetatoolbar-cgd'] = 1;
+$wgDefaultUserOptions['wikieditor-preview'] = 1;
+$wgDefaultUserOptions['wikieditor-publish'] = 1;
+
+wfLoadExtension( 'CategoryTree' );
+
+$wgCategoryTreeDefaultOptions['mode'] = 'pages';
+$wgCategoryTreeMaxDepth = array(CT_MODE_PAGES => 3, CT_MODE_ALL => 3, CT_MODE_CATEGORIES => 3);
+$wgCategoryTreeDefaultOptions['depth'] = 3;
+$wgCategoryTreeDefaultMode = CT_MODE_PAGES;
+$wgCategoryTreeDisableCache = 10;
+
+wfLoadExtension( 'Math' );
+
+$wgDefaultUserOptions['math'] = 'mathml';
+$wgMathFullRestbaseURL='https://api.formulasearchengine.com/';
+
+require_once "$IP/extensions/OpenID/OpenID.php";
+
+$wgOpenIDLoginOnly=false;
+$wgOpenIDMode='consumer';
+$wgOpenIDTrustEmailAddress=true;
+$wgOpenIDForcedProvider = 'https://stepik.org/openid/xrds/';
+$wgOpenIDAllowExistingAccountSelection = false;
+$wgOpenIDAllowAutomaticUsername = false;
+$wgOpenIDSmallLogoUrl = 'https://stepik.org/static/classic/ico/favicon_16.png';
+
+require_once "$IP/extensions/DeleteBatch/DeleteBatch.php";
+
+$wgGroupPermissions['bureaucrat']['deletebatch'] = false;
+$wgGroupPermissions['sysop']['deletebatch'] = true;
